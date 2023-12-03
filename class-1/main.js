1ad9bc5171533f8a1f4f7a1d38d3fe9ba129382e
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-12-03 21:41:10 +0530

    class 1

---
+++ class-1/main.js
@@ -0,0 +1,73 @@
+// console.log("Hello World");
+// console.log(a);
+//
+
+// let firstName = "Deep";
+// let age = 22;
+// let isMarried = true;
+
+// if (isMarried === true) {
+//   console.log(firstName + "is married");
+// } else {
+//   console.log(firstName + "is not married")
+// }
+
+// let ans = 0;
+
+// for (let i = 0; i<= 100; i++) {
+//   ans += i;
+// }
+
+// console.log(ans);
+//
+
+// const personArray = ["deep", "harkirat", "priya"];
+// const genderArray = ["male", "male", "female"];
+
+// for (let i =0; i< personArray.length; i++) {
+//   if(genderArray[i] === "male") {
+//     console.log(personArray[i]);
+//   }
+// }
+//
+
+// const allUser = [{
+//     firstName: "Deep",
+//     gender: "male"
+// }, {  
+//     firstName: "harkirat",
+//     gender: "male"
+// }, {
+//     firstName: "priya",
+//     gender: "female"
+//   }]
+
+// for(let i =0; i<= allUser.length; i++) {
+//   if(allUser[i]["gender"] == "male") {
+//     console.log(allUser[i]["firstName"]);
+//   }
+// }
+//
+
+// for (let i = 0; i<100000000000; i++) {
+//   sum = sum + i;
+//   
+// }
+
+// console.log(sum);
+
+function sum(num1, num2, funToCall) {
+    let result = num1 + num2;
+    // return result;
+  //
+}
+
+function displayResult(data) {
+    console.log("Result of the sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+// You are only allowed to call one function after this
