9da8ad31850cedc42d669c11e6114d3f6e5626e1
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-12-11 22:31:38 +0530

    formating go file

--- http-server-1/go/main.go
+++ http-server-1/go/main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"fmt"
 	"net/http"
@@ -8,41 +7,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request)  {
-  fmt.Fprint(w, "welcome to the home page!")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "welcome to the home page!")
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-  // extracr the "name"
-  vars := mux.Vars(r)
-  name := vars["name"]
+	// extracr the "name"
+	vars := mux.Vars(r)
+	name := vars["name"]
 
-  // send a greeting with extracted name
-  fmt.Fprint(w, "Hello, %s\n", name)
-
+	// send a greeting with extracted name
+	fmt.Fprint(w, "Hello, %s\n", name)
 
 }
 
 func main() {
-  // creating a new gorilla mux router
-  router := mux.NewRouter()
+	// creating a new gorilla mux router
+	router := mux.NewRouter()
 
-  // define routes
-  router.HandleFunc("/", homeHandler).Methods("GET")
-  router.HandleFunc("/hello/{name}", helloHandler).Methods("GET")
-  
-  //set up the http server with gorilla mux router
-  server := &http.Server{
-    Addr: ":8080",
-    Handler: router,
-  }
+	// define routes
+	router.HandleFunc("/", homeHandler).Methods("GET")
+	router.HandleFunc("/hello/{name}", helloHandler).Methods("GET")
 
-  //start the server
-  fmt.Println("Server is listening on :8080...")
-  err := server.ListenAndServe()
-  if err != nil {
-    fmt.Println("Error starting the server:", err)
-  }
+	//set up the http server with gorilla mux router
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	//start the server
+	fmt.Println("Server is listening on :8080...")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
 }
-
-
