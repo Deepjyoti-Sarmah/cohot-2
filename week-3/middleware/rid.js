3c949fa1689166ef2c08d03894990ed103d74a9c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-12-29 16:51:49 +0530

    middle ware using use command

--- week-3/middleware/rid.js
+++ week-3/middleware/rid.js
@@ -1,16 +1,16 @@
 const express = require("express");
-
 const app = express();
 
-function isOldEnough(age) {
-  if (age >= 14) {
-    return true;
-  }else {
-    return false;
-  }
-}
+// function isOldEnough(age) {
+//   if (age >= 14) {
+//     return true;
+//   }else {
+//     return false;
+//   }
+// }
 
 function isOldEnoughMiddleware(req, res, next) {
+  let age = req.query.age;
   if (age >= 14) {
     next(); 
   }else {
@@ -20,28 +20,55 @@ function isOldEnoughMiddleware(req, res, next) {
   }
 }
 
+app.use(isOldEnoughMiddleware)
+
 app.get("/ride2", function(req, res) {
-  if (isOldEnough(req.query.age)) {
+  // if (isOldEnough(req.query.age)) {
     res.json({
       msg: "you have successfully riden the ride 1"
     });
-  }else {
-    res.status(400).json({
-      msg: "sorry you are not at age yet"
-    });
-  }
+  // }else {
+    // res.status(400).json({
+      // msg: "sorry you are not at age yet"
+    // });
+  // }
 });
 
 app.get("/ride", function(req, res) {
-  if (isOldEnough(req.query.age)) {
+  // if (isOldEnough(req.query.age)) {
     res.json({
       msg: "you have successfully riden the ride 1"
     });
-  }else {
-    res.status(400).json({
-      msg: "sorry you are not at age yet"
-    });
-  }
+  // }else {
+  //   res.status(400).json({
+  //     msg: "sorry you are not at age yet"
+  //   });
+  // }
 });
+
+
+// app.get("/ride2", isOldEnoughMiddleware, function(req, res) {
+//   // if (isOldEnough(req.query.age)) {
+//     res.json({
+//       msg: "you have successfully riden the ride 1"
+//     });
+//   // }else {
+//     // res.status(400).json({
+//       // msg: "sorry you are not at age yet"
+//     // });
+//   // }
+// });
+
+// app.get("/ride", isOldEnoughMiddleware, function(req, res) {
+//   // if (isOldEnough(req.query.age)) {
+//     res.json({
+//       msg: "you have successfully riden the ride 1"
+//     });
+//   // }else {
+//   //   res.status(400).json({
+//   //     msg: "sorry you are not at age yet"
+//   //   });
+//   // }
+// });
 
 app.listen(3000);
