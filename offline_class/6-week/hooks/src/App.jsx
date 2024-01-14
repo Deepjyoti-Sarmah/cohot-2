73fa533eed24bf77c399a94e8775635c91f82a3d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-01-14 17:46:09 +0530

    useMemo

--- offline_class/6-week/hooks/src/App.jsx
+++ offline_class/6-week/hooks/src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
 function App() {
-  const [exchangeData, setExchangeData] = useState({});
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchange2Data, setExchange2Data] = useState({});
   const [bankData, setBankData] = useState({});
 
   // fetch("https://google.com", async (res) => {
@@ -12,19 +13,39 @@ function App() {
   //
   useEffect(() => {
     setTimeout(() => {
+      setExchange1Data({
+        returns: 100
+      });
+    });
+
+    setTimeout(() => {
+      setExchange2Data({
+        returns: 100
+      });
+    });
+
+    setTimeout(() => {
       setBankData({
         income: 300
       });
     }, 3000);
 
-    setTimeout(() => {
-      setExchangeData({
-        returns: 100
-      });
-    }, 1000);
   },[]);
 
-  const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
+  // const cryptoReturns = useMemo(() => {
+  //   return exchange1Data.returns + exchange2Data.returns ;
+  // }, [exchange1Data, exchange2Data]);
+  // const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
+  // const incomeTax = (bankData.income + cryptoReturns) * 0.3;
+
+  // function cryptoReturns() {
+  //   return exchange1Data.returns + exchange2Data.returns ;
+  // }
+
+  // const incomeTax = (cryptoReturns() + bankData.income) * 0.3;
+  // console.log(incomeTax);
+
+
 
   return (
     <div>
