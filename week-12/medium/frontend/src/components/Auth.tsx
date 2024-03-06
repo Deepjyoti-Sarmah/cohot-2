6eb39128ff4fc399d931f01f478f910aa472e593
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-03-07 00:47:26 +0530

    learning tailwindcss

---
+++ week-12/medium/frontend/src/components/Auth.tsx
@@ -0,0 +1,16 @@
+
+const Auth = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="text-3xl font-extrabold">
+        Create an account
+      </div>
+      <div className="text-slate-500">
+        <h2>Sign up</h2>
+
+      </div>
+    </div>
+  )
+}
+
+export default Auth
--- week-12/medium/frontend/src/pages/Signup.tsx
+++ week-12/medium/frontend/src/pages/Signup.tsx
@@ -1,34 +1,13 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@radix-ui/react-label"
+import Auth from "@/components/Auth"
 
 const Signup = () => {
   return (
-    <div className="w-full h-screen m-auto p-4 flex flex-row align-middle justify-center">
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <div>
-        <Card className="max-w-sm mx-auto">
-          <CardHeader>
-            <CardTitle className="text-2xl">Signup</CardTitle>
-            <CardDescription>Enter your email below to create your account.</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="m@example.com" required type="email" />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" required type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full">Sign up</Button>
-          </CardFooter>
-        </Card>
+        <Auth />
       </div>
-      <div>
-        <h1>Create an account</h1>
+      <div className="hidden lg:block">
+        Quote
       </div>
     </div>
   )
