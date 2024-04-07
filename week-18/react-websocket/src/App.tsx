598432a8b54b1081aa03eaa9ef5bb489ae60e98c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-07 21:33:02 +0530

    adding client and worker code for redis

---
+++ week-18/react-websocket/src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [latestMessage, setLatestMessage]= useState("");
+
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8080');
+    newSocket.onopen = () => {
+      console.log('Connection established');
+      newSocket.send('Hello Server!');
+    }
+    newSocket.onmessage = (message) => {
+      console.log('Message received:', message.data);
+      setLatestMessage(message.data)
+    }
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [])
+
+  if (!socket) {
+    return <div>
+      Connection to WebSocket
+    </div>
+  }
+
+  return (
+    <>
+      {latestMessage}
+    </>
+  )
+}
+
+export default App
